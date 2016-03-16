5f7b202da1fb164bb44434f074118c52cf04f311
Naldo Maranhão <regisousa7@gmail.com>
2016-03-16 10:29:25 -0300

    Primera comisión

---
+++ Pages/home.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/carrousel.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <title>Curió</title>
+</head>
+<body>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="img/slide1.jpg" >
+        </div>
+
+        <div class="item">
+            <img src="img/slide1.jpg" alt="Chania">
+        </div>
+
+        <div class="item">
+            <img src="img/slide1.jpg" alt="Flower">
+        </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <img class="seta1" href="#myCarousel" role="button" data-slide="prev" src="img/seta-left.png"/>
+
+    <img class="seta2" href="#myCarousel" role="button" data-slide="next" src="img/seta-right.png"/>
+</div>
+
+<div class="col-lg-12 divisao1"></div>
+
+<div class="col-lg-12 icones">
+    <div class="box">
+            <div class="icons">
+                <img class="imgIcons" src="img/icon1.jpg">
+                <div class="texto">
+                    <p class="title">Quero assinar já</p>
+                    <p class="info">Preencha o formulário e
+                        entraremos em contato
+                        em menos de 24 horas.
+                    </p>
+                </div>
+            </div>
+
+            <div class="icons">
+                <img class="imgIcons" src="img/iconLocalizacao.jpg">
+                <div class="texto">
+                    <p class="title">Nossa cobertura</p>
+                    <p class="info">Descubra onde encontrar
+                        internet de qualidade veja
+                        onde atendemos.
+                    </p>
+                </div>
+            </div>
+
+            <div class="icons">
+                <img class="imgIcons" src="img/iconPlanos.jpg">
+                <div class="texto">
+                    <p class="titleMaior">Os melhores planos para você</p>
+                    <p class="info">Temos planos e preços
+                        que cabem no seu bolso.
+                        É só escolher e assinar
+                    </p>
+                </div>
+            </div>
+
+            <div class="icons iconFinal">
+                <img class="imgIcons" src="img/iconContato.jpg">
+                <div class="texto">
+                    <p class="title">Fale conosco!</p>
+                    <p class="info">Informe-se, tire dúvidas, dê sugestões
+                        e contrate nossos serviços.
+                    </p>
+                </div>
+            </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ Pages/topo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/topo.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <title>Curió</title>
+</head>
+<body>
+<div id="box">
+<div class="col-lg-12 barraSuperior">
+    <div class="botao">
+        <button class="areaCliente">Área do cliente &nbsp;<span class="glyphicon glyphicon-user"></span></button>
+    </div>
+</div>
+
+<div class="col-lg-12 barraInferior">
+    <div class="logo">
+        <img id="logo" src="img/logo.jpg">
+    </div>
+
+    <div class="menu">
+        <ul class="nav nav-pills">
+            <li role="presentation" class="active"><a href="home">Home</a></li>
+            <li role="presentation"><a href="quemSomos">Quem somos</a></li>
+            <li role="presentation"><a href="planos">Planos</a></li>
+            <li role="presentation"><a href="contato">Contatos</a></li>
+            <li role="presentation"><a href="http://www.speedtest.net/pt/" target="_blank">Medidor de velocidade</a></li>
+        </ul>
+    </div>
+</div>
+</div>
+</body>
+</html>
